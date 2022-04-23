e842725471b8ae9fdcd9591a03ff0f976e1991de
Gabriel Henrique <77837886+GabrielHenriquedev@users.noreply.github.com>
2022-04-23 10:06:56 -0300

    Configuração inicial

---
+++ gymManage/src/main/java/com/gymManage/gymManage/GymManageApplication.java
@@ -0,0 +1,13 @@
+package com.gymManage.gymManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymManageApplication.class, args);
+	}
+
+}
