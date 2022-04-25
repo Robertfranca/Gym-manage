03e244e8c2ee1759e01f676433bd150a6ec2573a
Gabriel Henrique <77837886+GabrielHenriquedev@users.noreply.github.com>
2022-04-24 21:16:13 -0300

    frist controller

--- GymManage/src/test/java/com/gymManege/GymManage/GymManageApplicationTests.java
+++ GymManage/src/test/java/com/gymManege/GymManage/GymManageApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.gymManege.GymManage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GymManageApplicationTests {
 
 	@Test
 	void contextLoads() {
